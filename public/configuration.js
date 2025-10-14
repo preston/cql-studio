9a2038e49163ea17a67bc08dcc72b46d5a311ad0
Preston Lee <preston.lee@prestonlee.com>
2025-10-14 12:46:45 -0700

    Removing generated files

--- public/configuration.js
+++ public/configuration.js
@@ -1,6 +1,6 @@
 (function(window) {
     // Environment variables
-    window["CQL_STUDIO_RUNNER_BASE_URL"] = "";
+    window["CQL_STUDIO_RUNNER_BASE_URL"] = "https://cql-tests-runner.quality.hl7.org/";
     window["CQL_STUDIO_FHIR_BASE_URL"] = "";
     window["CQL_STUDIO_TRANSLATION_BASE_URL"] = "";
 })(this);
